8a37a512afaebc08ca929cd8e57a9ee8c0233b24
1TheCrazy <3thecrazy@gmail.com>
2025-09-29 16:09:22 +0200

    add different OS support

--- src/client/java/net/heartbeat/ShutdownHooker.java
+++ src/client/java/net/heartbeat/ShutdownHooker.java
@@ -6,6 +6,8 @@ import net.heartbeat.util.EmbeddedExtractor;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.Locale;
 
 public class ShutdownHooker {
     // We install a shutdown hook for this reason:
@@ -19,26 +21,63 @@ public class ShutdownHooker {
             try {
                 String pid = String.valueOf(ProcessHandle.current().pid());
 
-                // Get the Path to the current java.exe
-                String javaHome = System.getProperty("java.home");
-                Path javaBin = Paths.get(
-                        javaHome, "bin", System.getProperty("os.name").toLowerCase().contains("win") ? "java.exe" : "java"
-                );
-
                 // This may not show up in logs
                 Heartbeat.LOGGER.info("Starting Replacer... ");
 
+                // Get os specifics
+                String os = System.getProperty("os.name", "").toLowerCase(Locale.ROOT);
+
+                // Args passed
+                Path absoluteJarPath = Path.of(".\\.heartbeat\\" + EmbeddedExtractor.REPLACER_JAR_FILE_NAME).toAbsolutePath();
+                String gameDir = FabricLoader.getInstance().getGameDir().toString();
+                String removableJars = HeartbeatClient.getRemovableJarsString();
+
+                List<String> cmd;
+
+                if (os.contains("win")){
+                    // Get the Path to the current java.exe
+                    String javaHome = System.getProperty("java.home");
+                    Path javaBin = Paths.get(
+                            javaHome, "bin", System.getProperty("os.name").toLowerCase().contains("win") ? "java.exe" : "java"
+                    );
+
+                    cmd = List.of(
+                            "cmd.exe", "/c", "start", "", // detached new window
+                            "powershell", "-ExecutionPolicy", "Bypass", "-NoExit", "-Command",
+                            // Launch replacer JAR with the java bin we were just using
+                            javaBin.toString(), "-jar", absoluteJarPath.toString(),
+                            // 1st Arg: JVM process ID
+                            // 2nd Arg: Running dir of Game
+                            // 3rd Arg: String of jars that should be deleted ("mod1.jar,mod2.jar")
+                            pid, gameDir, removableJars
+                    );
+                } else if (os.contains("mac") || os.contains("darwin")) {
+                    // macOS
+                    String javaHome = System.getProperty("java.home");
+                    String javaCmd  = (javaHome != null)
+                            ? java.nio.file.Paths.get(javaHome, "bin", "java").toString()
+                            : "java";
+                    cmd = java.util.List.of(javaCmd, "-jar", absoluteJarPath.toString(), pid, gameDir, removableJars);
+
+                } else if (os.contains("nux") || os.contains("nix")) {
+                    // Linux/Unix
+                    String javaHome = System.getProperty("java.home");
+                    String javaCmd  = (javaHome != null)
+                            ? java.nio.file.Paths.get(javaHome, "bin", "java").toString()
+                            : "java";
+                    cmd = java.util.List.of(javaCmd, "-jar", absoluteJarPath.toString(), pid, gameDir, removableJars);
+
+                } else {
+                    // Fallback (desktop only)
+                    String javaHome = System.getProperty("java.home");
+                    String javaCmd  = (javaHome != null)
+                            ? java.nio.file.Paths.get(javaHome, "bin", "java").toString()
+                            : "java";
+                    cmd = java.util.List.of(javaCmd, "-jar", absoluteJarPath.toString(), pid, gameDir, removableJars);
+                }
+
                 // Launch DETACHED so it survives after JVM exits
-                new ProcessBuilder(
-                        "cmd.exe", "/c", "start", "", // detached new window
-                        "powershell", "-ExecutionPolicy", "Bypass", "-NoExit", "-Command",
-                        // Launch replacer JAR with the java bin we were just using
-                        javaBin.toString(), "-jar", ".\\.heartbeat\\" + EmbeddedExtractor.REPLACER_JAR_FILE_NAME,
-                        // 1st Arg: JVM process ID
-                        // 2nd Arg: Running dir of Game
-                        // 3rd Arg: String of jars that should be deleted ("mod1.jar,mod2.jar")
-                        pid, FabricLoader.getInstance().getGameDir().toString(), HeartbeatClient.getRemovableJarsString()
-                ).start();
+                new ProcessBuilder(cmd).start();
 
             } catch (Exception e) {
                 // This may not show up in logs
